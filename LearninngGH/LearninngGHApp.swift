bf5a7158785c078fd26a5bb80dd1e40294010243
Armand Graf <grar1012@gmail.com>
2022-05-19 11:20:05 +0200

    Initial Commit

---
+++ LearninngGH/LearninngGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearninngGHApp.swift
+//  LearninngGH
+//
+//  Created by Armand Graf on 19.05.22.
+//
+
+import SwiftUI
+
+@main
+struct LearninngGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
